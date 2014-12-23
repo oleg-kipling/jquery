9ad12b24c773b734bf2fd934ddf447f52aeb62ad
Oleg Kipling <oleg.kipling@gmail.com>
2014-12-24 00:07:14 +0300

    Parallax Scrolling

---
+++ Parallax Scrolling/gist.js
@@ -0,0 +1,23 @@
+opacity: .8;
+top: 0;
+list-style-type: none;
+
+z-index: 100,200,300,1000;
+
+#content article#home {
+	
+}
+
+<nav id="main-nav">
+	<ul>
+		<li><a class="moon" href="#moon">Moon</a></li>
+	</ul>
+</nav>
+
+<article id="moon">
+	<h2>Fly Me to the Moon</h2>
+	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+</article>
+
+.scrollTop();
+linear
